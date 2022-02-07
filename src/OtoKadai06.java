1a7536ae47acdd4c6039a112a83b8a2a45ddc9a5
200195 <200195@e0601-07.JC-21.local>
2022-02-07 11:26:53 +0900

    kadai6

--- src/OtoKadai06.java
+++ src/OtoKadai06.java
@@ -22,17 +22,19 @@ public class OtoKadai06  {
 		oto2.powerOn();
 		while(true) {
 			oto2.lightRed();
-			oto1.sleep();
 			oto1.lightGreen();
 			oto1.sleep(10);
 			oto1.lightYellow();
 			oto1.sleep(3);
 			oto1.lightRed();
 			oto1.sleep();
+			
 			oto2.lightGreen();
 			oto2.sleep(10);
 			oto2.lightYellow();
 			oto2.sleep(3);
+			oto2.lightRed();
+			oto2.sleep();
 		}
 	}
 }
